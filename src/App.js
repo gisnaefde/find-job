d5aff3ea7f2782a53edfe0fe3fd8238fc18d6dbb
gisnaefde <gisnaefde198@gmail.com>
2022-07-10 19:36:01 +0700

    Login Reaguster Change Password

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
 import Landing from './Page/Landing';
+import Login from './Page/Login';
+import Register from './Page/Register';
+import ChangePassword from './Page/ChangePassword';
 
 function App() {
   return (
     <div>
-      <Landing></Landing>
+      <ChangePassword/>
     </div>
 
   );
